89c5ae8f4d807f0856241550b7dc72efd0a5b7f3
Carlos Alvarado Martinez <45523697+calvarado2004@users.noreply.github.com>
2021-04-26 14:53:14 -0400

    Update portworx.sh
    fix script

--- portworx.sh
+++ portworx.sh
@@ -1,1 +1,5 @@
+kubectl apply --kubeconfig=/etc/kubernetes/admin.conf -f 'https://install.portworx.com/2.6?comp=pxoperator'
+
+sleep 30s
+
 kubectl apply --kubeconfig=/etc/kubernetes/admin.conf -f /tmp/portworx-enterprise.yaml
