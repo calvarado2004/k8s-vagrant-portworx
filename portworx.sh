36c5dc16b0ecf9d87082f18bbe1bcfb4a28c463e
Carlos Alvarado Martinez <45523697+calvarado2004@users.noreply.github.com>
2021-04-26 15:10:01 -0400

    Update portworx.sh
    Wait 180s to let the operator to create the necessary objects.

--- portworx.sh
+++ portworx.sh
@@ -1,5 +1,5 @@
 kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f 'https://install.portworx.com/2.6?comp=pxoperator'
 
-sleep 30s
+sleep 180s
 
 kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f /tmp/portworx-enterprise.yaml
